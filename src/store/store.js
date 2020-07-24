59fb3a11ba50d6e49a0e1cd664d0497a7f2793ac
marah <marahabdallah91@gmail.com>
2020-07-24 10:53:58 +0300

    set up

---
+++ src/store/store.js
@@ -0,0 +1,13 @@
+import { createStore, combineReducers,applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+
+
+
+const reducers = combineReducers({  });
+
+const store = () => {
+  return createStore(reducers, composeWithDevTools(applyMiddleware(thunk)));
+};
+
+export default store();
