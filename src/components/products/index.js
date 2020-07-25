6edadeabb668731e847cf610a005769a69e7f75d
yasminadaileh1 <60566993+yasminadaileh1@users.noreply.github.com>
2020-07-25 15:21:46 +0300

    working on

--- src/components/products/index.js
+++ src/components/products/index.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React , { useEffect } from 'react';
 import {connect} from 'react-redux';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import products from '../../store/reducers/products';
-
+import { getRemoteData} from '../../store/reducers/products.js';
 function Products (props){
+    const fetchData = () => {
+        props.get();
+      };
+      useEffect(() => {
+        // superagent.get()
+        fetchData();
+      }, []);
     return (
         <section>
+              <button onClick={fetchData}>Get Products</button>
             <ul className='pro'>
                 {
-                    props.products.products.map(product =>{
+                    props.data.products.map(product =>{
+                        console.log('hello')
                         return (
         <Card style={{ width: '18rem' }}className = {`cards ${product.title}`} key = {product.title}>
             <Card.Img className='imagepro' variant="top" src={product.img} />
@@ -40,6 +49,10 @@ function Products (props){
     )
 }
 const mapStateToProps = (state) =>{
-    return {products : state.Products}
+    return {data : state.data}
 } 
-export default connect(mapStateToProps )(Products);
+
+  const mapDispatchToProps = (dispatch) => ({
+    get: () => dispatch(getRemoteData()),
+  });
+export default connect(mapStateToProps  , mapDispatchToProps)(Products);
--- src/store/reducers/products.js
+++ src/store/reducers/products.js
@@ -1,39 +1,60 @@
 import superagent from 'superagent';
 
 const initialState = {
-    products : [ ],
+    products : [],
 }
 export default (state = initialState ,action) =>{
     const { type , payload } = action;
     switch(type){
         case 'ACTIVE' :
-            let products = initialState.products.filter(product=> product.category === payload) ;
+            // let products = initialState.products.filter(product=> product.category === payload) ;
             return {products :  products};
-
-    
+        case 'setPosts':
+            let products = initialState.products
+            return {...state,products : payload};
+        case 'GET':
+            console.log('GET', payload);
+            return payload;
         default:
              return state;
     }
 }
-export const active = (categoryName) => {
-    return {
-        type : 'ACTIVE',
-        payload : categoryName,
-    }
-}
+// export const active = (categoryName) => {
+//     return {
+//         type : 'ACTIVE',
+//         payload : categoryName,
+//     }
+// }
 
-export const getPosts = (categories) => dispatch => {
-    let api = `https://trader401.herokuapp.com/categories/${categories}`;
-    return superagent.get(api)
-      .then(data => {
-        //   (console.log(data.body , 'daata.body'))
-        dispatch(getActionProducts(data.body))
+// export const getPosts  = dispatch => {
+//     let api = `https://trader401.herokuapp.com/all`;
+//     return superagent.get(api)
+//       .then(data => {
+//           (console.log(data.body , 'daata.body'))
+//           dispatch(setPosts(data.body));
+//       });
+//   };
+
+//   export const setPosts = payload => {
+//     return {
+//       type: 'setPosts',
+//       payload: payload,
+//     };
+//   };
+
+  export const getRemoteData = function () {
+    let api = `https://trader401.herokuapp.com/all`;
+    return (dispatch) => {
+      return superagent.get(api).then((response) => {
+        dispatch(getAction({ products: response.body }));
       });
+    };
   };
-
-  export const getActionProducts = payload => {
+  
+  
+  export const getAction = (payload) => {
     return {
       type: 'GET',
-      payload: payload
-    }
-  }
+      payload: payload,
+    };
+  };
