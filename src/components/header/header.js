cf23e9d629385b8dc8ec1980522a74fdd608fce0
yasminadaileh1 <60566993+yasminadaileh1@users.noreply.github.com>
2020-07-31 14:26:23 +0300

    header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Navbar,Form,FormControl,Button,Nav,DropdownButton, Dropdown} from 'react-bootstrap';
+import {Navbar,Nav, NavItem ,NavDropdown} from 'react-bootstrap';
 import './header.scss'
 import { connect } from 'react-redux';
 import * as actions2 from '../../store/reducers/profile';
@@ -12,68 +12,25 @@ function Header(props) {
 
     <>
       <Navbar bg="light" expand="lg" className="header">
-        <Navbar.Brand href="#home"> T R A D E R </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Brand href="/"> T R A D E R </Navbar.Brand>
         <Navbar.Collapse id="basic-navbar-nav">
 
           <Nav className="mr-auto">
             
             <NavLink className="a-tag" to="/">Home</NavLink>
+            <NavDropdown className="a-tag" title="categories" id="basic-nav-dropdown">
+        <NavDropdown.Item  href="#action/3.1">Electronics</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
             <NavLink className="a-tag" to={`/user/${props.username}`} onClick={() => {
               props.getUser(props.username);
               props.getPosts(props.username);
-            }} >Profile</NavLink>
-            <NavLink className="a-tag" to="/log">Log In</NavLink>
-            <NavLink className="a-tag" to="/post">Add Post</NavLink>
+            }} >{props.username || 'Profile'}</NavLink>
             <NavLink className="a-tag" to="/admin">Admin</NavLink>
+            <NavLink className="a-tag" to="/log">Log In</NavLink>
           </Nav>
-          <div class="search-bar  p-3 p-lg-1 pl-lg-4">
-              
-          <Form action="#">
-                <div className="row">
-                  <div className="col-lg-4 d-flex align-items-center form-group">
-                  <FormControl type="text" placeholder="Search 🔍" className="mr-sm-2 form-control border-0 shadow-0" />
-                  </div>
-                  <div className="col-lg-3 d-flex align-items-center form-group no-divider">
-                  <DropdownButton
-              alignRight
-              title="Categories"
-              id="dropdown-menu-align-right"
-              className="dropdown bootstrap-select selectpicker"
-            >
-              <Dropdown.Item eventKey="1">cars</Dropdown.Item>
-              <Dropdown.Divider />
-              <Dropdown.Item eventKey="2">electronics</Dropdown.Item>
-              <Dropdown.Divider />
-              <Dropdown.Item eventKey="3">mobiles</Dropdown.Item>
-              <Dropdown.Divider />
-              <Dropdown.Item eventKey="4">furniture</Dropdown.Item>
-          </DropdownButton>
-          </div>
-                  <div className="col-lg-2">
-                    <Button className="btn btn-primary btn-block rounded-xl h-100" type="submit">Search </Button>
-                  </div>
-                </div>
-              </Form>
-              </div>
-
-
-
-          {/* <Form inline>
-            <FormControl type="text" placeholder="Search 🔍" className="mr-sm-2" />
-            <DropdownButton
-              alignRight
-              title="Categories"
-              id="dropdown-menu-align-right"
-            >
-              <Dropdown.Item eventKey="1">cars</Dropdown.Item>
-              <Dropdown.Item eventKey="2">electronics</Dropdown.Item>
-              <Dropdown.Item eventKey="3">mobiles</Dropdown.Item>
-              <Dropdown.Divider />
-              <Dropdown.Item eventKey="4">furniture</Dropdown.Item>
-          </DropdownButton>
-            <Button variant="outline-primary" className="buttonn">Search</Button>
-          </Form> */}
         </Navbar.Collapse>
       </Navbar>
 
