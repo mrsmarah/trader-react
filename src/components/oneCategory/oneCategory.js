43ff57ad8bb14bc96fef4b077e4e20240e04e2e0
marah <marahabdallah91@gmail.com>
2020-08-03 00:57:56 +0300

    add one catgory img

--- src/components/oneCategory/oneCategory.js
+++ src/components/oneCategory/oneCategory.js
@@ -9,11 +9,11 @@ import Parallax from './parallax.js';
 const OneCategory = (props) => {
 
   let{category} = useParams();
-  
+  console.log('props.categories',props.categories);
   
   useEffect(() => {
     console.log('CATEGORY',category);
-    props.getCategory(category);
+    props.getCategory(category, props.categories.activeCategoryImg);
     
   },[]);
 
@@ -22,10 +22,9 @@ const OneCategory = (props) => {
       <div>
         <Parallax/>
         <span>
-        <h4>{ props.categories.activeCategory}</h4>
-
+          <h4>{ props.categories.activeCategory}</h4>
         </span>
-        
+        <img src={ props.categories.activeCategoryImg} alt='active Category Img'/>
       </div>
 
     </>
@@ -39,7 +38,7 @@ const mapStateToProps = (state) => {
 };
   
 const mapDispatchToProps = (dispatch) => ({
-  getCategory: (category) => dispatch(handelCategory(category)),
+  getCategory: (category, img) => dispatch(handelCategory(category , img)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(OneCategory);
